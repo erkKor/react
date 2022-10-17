500cf53e32d23bc717573a2523f161a441b8dc59
Erik Körberg <korbergerik@gmail.com>
2022-10-17 16:32:20 +0200

    started on fixing contactform

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -5,19 +5,19 @@ const ContactForm = () => {
     <section className="contact-form">
         <div className="container">
             <h3>Come in Contact with Us</h3>
-            <form onsubmit="submitForm(event)" className="grid" novalidate>
+            <form onsubmit="submitForm(event)" novalidate>
                 <div>
                     <input id="name" onkeyup="validate(event)" type="text" placeholder="Your Name" className="error" />
-                    <span id="nameErrorMessage" className="errorMessage"></span>
+                    <div id="nameErrorMessage" className="errorMessage"></div>
                 </div>
                 <div>
                     <input id="email" onkeyup="validate(event)" type="email" placeholder="Your Email" />
-                    <span id="emailErrorMessage" className="errorMessage"></span>
+                    <div id="emailErrorMessage" className="errorMessage"></div>
                 </div>
                 <div>
                     <textarea id="comment" onkeyup="validate(event)" className="textarea"name="" cols="30" rows="10" placeholder="Comments"></textarea>
-                    <span id="commentErrorMessage" className="errorMessage"></span>
-                </div>
+                    <div id="commentErrorMessage" className="errorMessage"></div>
+                    </div>
                 <button className="button-theme" type="submit">Post Comments</button>
             </form>
         </div>
