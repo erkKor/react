0960c22a942601eff0759e538d6ffb265ba5e89b
Erik Körberg <korbergerik@gmail.com>
2022-10-18 14:24:18 +0200

    fixed up navbar with grid and reworked responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import MenuIcon from './items/MenuIcon'
 
 const Navbar = ({headerType}) => {
+    const [showMenu, setShowMenu] = useState(false)
+
+    const toggleMenu = () =>{
+        setShowMenu(!showMenu)
+    }
+
   return (
     <header className={headerType}>
         <nav className="container">
             <div className="brand">
                 <NavLink>Fixxo.</NavLink>
             </div>
-            <div className="menu-categories">
+            <div className={`menu-categories ${showMenu ? "d-grid": ""}`}>
                 <NavLink to="/" end>Home</NavLink>
                 <NavLink to="/categories" end>Categories</NavLink>
                 <NavLink to="/products" end>Products</NavLink>
@@ -17,10 +23,10 @@ const Navbar = ({headerType}) => {
             </div>
             <div className="icon-links">
                 <MenuIcon link="/search" icon="fa-light fa-magnifying-glass"/>
-                <MenuIcon link="/compare" icon="fa-light  fa-code-compare rotate-icon rotate-icon"/>
-                <MenuIcon link="/wishlist" icon="fa-light fa-heart" badge="badge" badgeNumber="1"/>
+                <MenuIcon hideOnMobile={true} link="/compare" icon="fa-light  fa-code-compare rotate-icon rotate-icon"/>
+                <MenuIcon hideOnMobile={true} link="/wishlist" icon="fa-light fa-heart" badge="badge" badgeNumber="1"/>
                 <MenuIcon link="/shoppingcart" icon="fa-light fa-bag-shopping" badge="badge" badgeNumber="3"/>
-                <button id="" className="icon-link btn-icon-link">
+                <button onClick={toggleMenu} id="" className="icon-link btn-icon-link">
                     {/* <i className="fa-light fa-bars"></i> */}
                     <i className="fa-light fa-bars-staggered"></i>
                 </button>
--- src/components/items/MenuIcon.js
+++ src/components/items/MenuIcon.js
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom'
 
 const MenuIcon = ({link, icon, badge, badgeNumber, hideOnMobile}) => {
   return (
-    <NavLink className={ `icon-link ${hideOnMobile ? "d-none" :""}`} to={link} end>
+    <NavLink className={ `icon-link ${hideOnMobile ? "d-none d-md-flex" : "" }` } to={link} end>
         <i className={icon}></i>
         <span className={badge}>{badgeNumber}</span>
     </NavLink>
