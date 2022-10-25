25894ed9c96f0e3c04ab56b16354119a97cb6ce0
Erik Körberg <korbergerik@gmail.com>
2022-10-25 15:28:00 +0200

    remade Tab, and started working on styling again

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -5,43 +5,6 @@ import Button from './items/Button'
 import SelectList from './items/SelectList'
 import Tab from './items/Tab'
 
-const tabContent = [
-    {
-        title: "Description",
-        content: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type 
-        and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, 
-        remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
-        and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`,
-    },
-    {
-        title: "Additional",
-        content: `ABU DHABI the printing and typesetting industry. 
-        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type 
-        and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, 
-        remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
-        and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`,
-    },
-    {
-        title: "Shopping & Returns",
-        content: `NEW YORK FOR SIKFJKS IS THE BVEST. 
-        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type 
-        and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, 
-        remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
-        and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`,
-    },
-    {
-        title: "Reviews",
-        content: `NEW YORK FOR SIKFJKS IS THE BVEST. 
-        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type 
-        and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, 
-        remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
-        and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`,
-    },
-
-]
-
-
 const ProductDetails = () => {
   return (
     <section className='product-details'>
@@ -91,13 +54,7 @@ const ProductDetails = () => {
                 </div>
             </div>
             <div>
-                <Tab>
-                    {tabContent.map((tab,idx) => (
-                    <Tab.TabContent key={`Tab-${idx}`} tab={tab.title}>{tab.content}</Tab.TabContent>
-                    ))
-
-                    }
-                </Tab>
+                <Tab/>
             </div>
         </div>
     </section>
--- src/components/items/Tab.js
+++ src/components/items/Tab.js
@@ -1,51 +1,101 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 
-const Tab = ({children, active = 0}) => {
-    const [activeTab, setActiveTab] = useState(active);
-    const [tabsData, setTabsData] = useState([]);
+const NewTab = () => {
+    const [toggleState, setToggleState] = useState(1);
 
-    useEffect(() => {
-        let data = [];
+        const toggleTab = (index) => {
+            setToggleState(index);
+        }
+ 
+  return (
+    <div className="container-tab">
+      <div className="bloc-tabs">
+        <button
+          className={toggleState === 1 ? "tabs active-tabs" : "tabs"}
+          onClick={() => toggleTab(1)}
+        >
+          Description
+        </button>
+        <button
+          className={toggleState === 2 ? "tabs active-tabs" : "tabs"}
+          onClick={() => toggleTab(2)}
+        >
+          Additional
+        </button>
+        <button
+          className={toggleState === 3 ? "tabs active-tabs" : "tabs"}
+          onClick={() => toggleTab(3)}
+        >
+            Shopping & Returns
+        </button>
+        <button
+        id="review"
+          className={toggleState === 4 ? "tabs active-tabs review" : "tabs review"}
+          onClick={() => toggleTab(4)}
+        >
+          Reviews
+        </button>
+      </div>
 
-        React.Children.forEach(children, element =>{
-            if(!React.isValidElement(element)) return;
-
-            const {props: {tab, children}} = element;
-            data.push({tab, children});
-        })
-
-        setTabsData(data);
-    }, [children])
-    return (
-        <div>
-            <ul className="tab-menu">
-                {
-                    tabsData.map(({tab}, idx) => (
-                        <li className="tab-item">
-                            <button className={`tab-link ${idx === active ? "active" : ""}`} 
-                            href="#"
-                            onClick={()=> setActiveTab(idx)}
-                            >
-                                {tab}
-                            </button>
-                        </li>
-                    ))
-                }
-            </ul>
-
-            <div className="tab-content">
-                {tabsData[activeTab] && tabsData[activeTab].children}
-            </div>
+      <div className="content-tabs">
+        <div
+          className={toggleState === 1 ? "content  active-content" : "content"}
+        >
+          <p>
+            Way extensive and dejection get delivered deficient sincerity gentleman age. Too end instrument possession contrasted motionless. 
+            Calling offence six joy feeling. Coming merits and was talent enough far. Sir joy northward sportsmen education. 
+            Discovery incommode earnestly no he commanded if. Put still any about manor heard.
+            <br/>
+            <br/>
+            * Village did removed enjoyed explain nor ham saw calling talking.
+            <br/>
+            * Securing as informed declared or margaret.
+            <br/>
+            * Joy horrible moreover man feelings own shy. 
+            <br/>
+            <br/>
+            On even feet time have an no at. Relation so in confined smallest children unpacked delicate. Why sir end believe uncivil respect. 
+            Always get adieus nature day course for common. My little garret repair to desire he esteem. 
+          </p>
         </div>
-      )
+
+        <div
+          className={toggleState === 2 ? "content  active-content" : "content"}
+        >
+          <h2>Content 2</h2>
+          <hr />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+            voluptatum qui adipisci.
+          </p>
+        </div>
+
+        <div
+          className={toggleState === 3 ? "content  active-content" : "content"}
+        >
+          <h2>Content 3</h2>
+          <hr />
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos sed
+            nostrum rerum laudantium totam unde adipisci incidunt modi alias!
+            Accusamus in quia odit aspernatur provident et ad vel distinctio
+            recusandae totam quidem repudiandae omnis veritatis nostrum
+            laboriosam architecto optio rem, dignissimos voluptatum beatae
+            aperiam voluptatem atque. Beatae rerum dolores sunt.
+          </p>
+        </div>
+        <div
+          className={toggleState === 4 ? "content  active-content" : "content"}
+        >
+          <h2>Content 4</h2>
+          <hr />
+          <p>
+            Cont 4
+          </p>
+        </div>
+      </div>
+    </div>
+  )
 }
 
-  
-
-const TabContent = ({children}) => {
-  return{children} 
-}
-
-Tab.TabContent = TabContent;
-
-export default Tab
+export default NewTab
